5e5b41f805a58fe830b277b87d0fd5c5bbda50c5
root <root@ip-172-31-35-191.ap-northeast-1.compute.internal>
2019-12-12 04:42:14 +0000

    on EC2

--- flaskr/config.py
+++ flaskr/config.py
@@ -1,2 +1,2 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3314/app?charset=utf8'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:55itolab!!@database-2.cm99sxzjgl23.ap-northeast-1.rds.amazonaws.com:3306/app?charset=utf8'
 SECRET_KEY = 'secret key'
